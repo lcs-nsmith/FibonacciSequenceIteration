64963be6a952e5570599e3e50593da7a95e4d400
Nathan Smith <nsmith@lcs.on.ca>
2022-11-15 15:06:39 -0500

    incredibly done, finds the sequence quickly and to a high degree, fixed the decimal formatting

--- FibonacciSequence/main.swift
+++ FibonacciSequence/main.swift
@@ -8,14 +8,29 @@
 import Foundation
 import Cocoa
 
-var f = 0
-var g = 1
-var h = 420
+//var f = 0
+//var g = 1
+//var h = 420
+//print(f)
+//print(g)
+//for i in  1...91 {
+//    h = g
+//    g = g + f
+//    f = h
+//    print(g)
+//}
+
+var f: Double = 0
+var g: Double = 1
+var h: Double = 420
 print(f)
 print(g)
-for i in  1...91 {
+for i in  1...1476 {
     h = g
     g = g + f
     f = h
-    print(g)
+    let text = String(format: "%.0f", g)
+    print("")
+    print(text)
 }
+
