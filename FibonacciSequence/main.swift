c6edcb8b6f8c9c04f2f18d6ba07a0aa853d92ee6
Nathan Smith <nsmith@lcs.on.ca>
2022-11-15 10:07:41 -0500

    done

--- FibonacciSequence/main.swift
+++ FibonacciSequence/main.swift
@@ -6,6 +6,32 @@
 //
 
 import Foundation
+import Cocoa
 
-print("Hello, World!")
+//func FibonacciSequence(with j: inout Int) {
+//    var oink: Int = 1
+//    var p: Int = 0
+//
+//    while p >= oink {
+//        j = 0
+//        print(j)
+//        for i in 1...10 {
+//            j = i + j  //(j = j + i)
+//        }
+//    }
+//
 
+var f = 0
+var g = 1
+var h = 420
+print(f)
+print(g)
+for i in  1...91 {
+    h = g
+    g = g + f
+    f = h
+    print(g)
+}
+
+//var n = 1
+//FibonacciSequence(with: &n)
